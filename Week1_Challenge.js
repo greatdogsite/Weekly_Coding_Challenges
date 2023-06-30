fc469e96f64c7d081a609f2b302ff8178f748710
Mango <greatdogsite@gmail.com>
2023-06-29 19:11:28 -0500

    Week 1 Complete - Area of a Triangle

---
+++ Week1_Challenge.js
@@ -0,0 +1,10 @@
+function areaOfTriangle(base, height){
+    return (base*height)/2;
+  }
+  
+  let area = areaOfTriangle(2,3);
+  console.log(area);
+
+  let triarea = (b,h) => (b*h)/2;
+  console.log(triarea(2,3));
+  
