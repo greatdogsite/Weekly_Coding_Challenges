b91ac78548605f7281e4a341fe226070535a47b9
Mango <greatdogsite@gmail.com>
2023-08-14 19:50:12 -0500

    Week 6 Complete - Count Characters in a String

--- Week6_Challenge.js
+++ Week6_Challenge.js
@@ -1,4 +1,12 @@
-function after(){
-    console.log("coding it afer class");
-
+function charCount(myChar, str) {
+	let array = str.split("");
+	sum = 0;
+	for (let letter of array){
+		if (letter === myChar){
+			sum++;			
+		}
+	}
+	return sum;
 }
+
+console.log(charCount("i", "I like fish"))
